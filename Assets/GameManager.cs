c10e92f678c536068cb1d649865a28e5396c3d42
eric [itb] rodriguez luque <eric.rodriguez.7e6@itb.cat>
2024-03-01 15:43:48 +0000

    Sort y Search acabados

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -8,13 +8,12 @@ using TMPro;
 public class GameManager : MonoBehaviour
 { 
     public List<Fakemon> fakemons;
+    public List<Fakemon> fakemonsFiltered;
     public List<Sprite> pixelArts;
     public List<GameObject> models;
     public GameObject modelInstance;
     public GameObject stage3d;
     public TMP_FontAsset typography;
-
-    public int actualSort;
 
     public static GameManager instance;
 
@@ -22,6 +21,7 @@ public class GameManager : MonoBehaviour
     void Awake()
     {
         fakemons = SQLConn.GetFakemon();
+        fakemonsFiltered = fakemons;
         if (instance == null)
         {
             instance = this;
@@ -43,16 +43,6 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         ChangeTypo();
-        /*
-        if (Input.GetKeyDown("1"))
-            this.Sort(0);
-        if (Input.GetKeyDown("2"))
-            this.Sort(1);
-        if (Input.GetKeyDown("3"))
-            this.Sort(2);
-        if (Input.GetKeyDown("4"))
-            this.Sort(3);
-        */
     }
     public void SetModel(string name)
     {
@@ -61,34 +51,6 @@ public class GameManager : MonoBehaviour
         if (modelInstance != null) Destroy(modelInstance);
 
         modelInstance = Instantiate(result.Count > 0 ? result[0] : models[0], stage3d.transform);
-    }
-    public List<Fakemon> Sort(int method)
-    {
-        var sortedList = new List<Fakemon>();
-
-        switch (method)
-        {
-            case 0:
-                sortedList = fakemons.OrderBy(x => x.id).ToList();
-                break;
-            case 1:
-                sortedList = fakemons.OrderBy(x => x.fakename).ToList();
-                break;
-            case 2:
-                sortedList = fakemons.OrderBy(x => x.season).ToList();
-                break;
-            case 3:
-                sortedList = fakemons.OrderBy(x => x.type).ToList();
-                break;
-        }
-
-        if (actualSort == method)
-        {
-            sortedList.Reverse();
-            actualSort = 20;
-        } else actualSort = method;
-
-        return sortedList;
     }
 
     public void Show(int method)
