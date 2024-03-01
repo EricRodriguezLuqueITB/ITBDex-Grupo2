36ab476cd18c60f2c6a655f7e86bc7fd14c430cc
DidacSanchez <didac618@gmail.com>
2024-03-01 16:58:58 +0100

    Rotacion en Zoom

--- Assets/Scripts/ConstantRotation.cs
+++ Assets/Scripts/ConstantRotation.cs
@@ -4,8 +4,37 @@ using UnityEngine;
 
 public class ConstantRotation : MonoBehaviour
 {
-    void Update()
+    private Touch touch;
+    private Vector2 touchPosition;
+    private Quaternion rotationY;
+    private float rotateSpeedModifier = 0.4f;
+    private bool zoom = true;
+
+    private void FixedUpdate()
     {
-        transform.Rotate(new Vector3( 0, 0.3f, 0), Space.World);
+        if (zoom)
+        {
+            transform.Rotate(new Vector3(0, 0.3f, 0), Space.World);
+        }
+        else
+        {
+            if (Input.touchCount > 0)
+            {
+                touch = Input.GetTouch(0);
+            }
+            if (touch.phase == TouchPhase.Moved)
+            {
+                rotationY = Quaternion.Euler(
+                    0f,
+                    -touch.deltaPosition.x * rotateSpeedModifier,
+                    0f);
+                transform.rotation = rotationY * transform.rotation;
+            }
+        }
+
+    }
+    public void changeZoom()
+    {
+        zoom = !zoom;
     }
 }
