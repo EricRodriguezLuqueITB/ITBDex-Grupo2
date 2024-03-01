561245542e889c95e1562ca609e0c1588e67b32b
eric [itb] rodriguez luque <eric.rodriguez.7e6@itb.cat>
2024-03-01 15:56:09 +0000

    Animacion ubicacion

--- Assets/Scripts/Ubication.cs
+++ Assets/Scripts/Ubication.cs
@@ -9,15 +9,35 @@ public class Ubication : MonoBehaviour
     [SerializeField] private Transform marker;
     [SerializeField] private GameObject profile;
     private string season;
+    private float clock;
+    private int movement;
     private void OnEnable()
     {
+        movement = 5;
+        transform.localPosition = new Vector3(0, transform.localPosition.y + movement, 0);
         season = profile.GetComponent<Profile>().fk.season;
         SetMarker();
+    }
+    private void Update()
+    {
+        if (Time.time - clock > 0.2f)
+        {
+            switch (transform.localPosition.y)
+            {
+                case <= 0:
+                case >= 20:
+                    movement *= -1;
+                    break;
+            }
+
+            transform.localPosition = new Vector3 ( 0, transform.localPosition.y + movement, 0);
+
+            clock = Time.time;
+        }
     }
     public void SetMarker()
     {
         List<Transform> ubis = ubications.Where(item => item.name == "Ubi" + season).ToList();
-        Debug.Log("Buscando: Ubi" + season);
         if (ubis.Count() == 0)
         {
             marker.position = new( 0, 0, 5);
