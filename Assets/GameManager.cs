f0f5affba004eadda120e18059f4dcf73f1636d2
eric [itb] rodriguez luque <eric.rodriguez.7e6@itb.cat>
2024-03-04 17:12:52 +0100

    Frames
    + Ajustado trigger para el pixelart de la dex

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,6 +7,7 @@ using TMPro;
 using System.IO;
 using UnityEngine.Android;
 using System.Text;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 { 
@@ -16,9 +17,11 @@ public class GameManager : MonoBehaviour
     public List<Sprite> pixelArts;
     public List<GameObject> models;
     public List<Sprite> seasonIcons;
+    public List<Sprite> seasonFrames;
 
     public GameObject modelInstance;
     public GameObject stage3d;
+    public GameObject stageFrame;
     public TMP_FontAsset font;
 
     public static GameManager instance;
@@ -61,6 +64,15 @@ public class GameManager : MonoBehaviour
         if (modelInstance != null) Destroy(modelInstance);
 
         modelInstance = Instantiate(result.Count > 0 ? result[0] : models[0], stage3d.transform);
+
+        string season = fakemons.Where(item => item.fakename.Contains(name)).ToList()[0].season;
+
+        if (seasonFrames.Where(item => item.name.Contains(season)).ToList().Count > 0)
+        {
+            stageFrame.GetComponent<Image>().color = new Color(255, 255, 255, 1);
+            stageFrame.GetComponent<Image>().sprite = seasonFrames.Where(item => item.name.Contains(season)).ToList()[0];
+        }
+        else stageFrame.GetComponent<Image>().color = new Color(0, 0, 0, 0);
     }
     public void CloseGame()
     {
