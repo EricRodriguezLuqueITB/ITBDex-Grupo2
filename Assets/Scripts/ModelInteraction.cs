3267fa04030ce7be4c53b6a4fcf6b1524fa8bccb
eric [itb] rodriguez luque <eric.rodriguez.7e6@itb.cat>
2024-03-05 15:50:55 +0000

    En proceso Idle audio

--- Assets/Scripts/ModelInteraction.cs
+++ Assets/Scripts/ModelInteraction.cs
@@ -8,6 +8,14 @@ public class ModelInteraction : MonoBehaviour
 {
     private Touch touch;
     private Vector2 startPosition;
+
+    private void Start()
+    {
+        List<Sound> sounds = AudioManager.instance.sounds.Where(item => item.name.ToLower().Contains(name.Split('(')[0]) && item.name.ToLower().Contains("idle")).ToList();
+
+        if (sounds.Count > 0) AudioManager.instance.ChoosePlay(sounds[0].name, 0);
+
+    }
     private void Update()
     {
         if (Input.touchCount > 0 && GameManager.instance.CheckZoom())
@@ -28,7 +36,18 @@ public class ModelInteraction : MonoBehaviour
 
     private void Interact()
     {
-        //Debug.Log(AudioManager.instance.sounds.Where(item => item.name.Contains(name.Split('(')[0])).ToList().Count);
-        AudioManager.instance.ChoosePlay(AudioManager.instance.sounds.Where(item => item.name.Contains(name.Split('(')[0])).ToList()[0].name, 0);
+        List<Sound> sounds = AudioManager.instance.sounds.Where(item => item.name.Contains(name.Split('(')[0])).ToList();
+
+        switch (sounds.Count)
+        {
+            case 1:
+                AudioManager.instance.ChoosePlay(sounds[0].name, 0);
+                break;
+            case > 1:
+
+                List<Sound> soundsF = sounds.Where(item => item.name.ToLower().Contains("attack")).ToList();
+                if(soundsF.Count == 1) AudioManager.instance.ChoosePlay(soundsF[0].name, 0);
+                break;
+        }
     }
 }
